aec665dc98b1609e0501294fd0986035356b11d3
afzalsho <makhkamov.afzalshokh@imon.tj>
2025-01-23 11:18:05 +0500

    first commit

--- config/products.php
+++ config/products.php
@@ -2,6 +2,6 @@
 
 return [
     'role' => 'admin', // Или 'user'
-    'email' => 'afzalshohmakhkamov02@gmail.com',
+    'email' => 'test@example.com',
     'webhook' =>'https://webhook.site/e46da57d-9eec-4b30-883d-d8f51f4e4b21', // URL webhook по умолчанию для тестирования
 ];
